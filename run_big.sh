5079c54d6c70e2ceae36c2900bbb520c9c7a0928
Augusto Barth <augustobarth@hotmail.com>
2023-12-12 15:28:47 -0300

    Adding jobs to bash

--- run_big.sh
+++ run_big.sh
@@ -10,6 +10,9 @@ for ((img = 1; img < count; img = img+1))
 do
     for i in {1..8}
     do
-        ./lsbStegDiffuse in/"imag_"$img".png" out/big/"out_"$img"_"$i"b.png" message_g.txt $i $1
+        ./lsbStegDiffuse in/"imag_"$img".png" out/big/"out_"$img"_"$i"b.png" message_g.txt $i $1 &
     done
 done
+
+wait
+echo "Done"
--- run_small.sh
+++ run_small.sh
@@ -10,6 +10,9 @@ for ((img = 1; img < count; img = img+1))
 do
     for i in {1..8}
     do
-        ./lsbStegDiffuse in/"imag_"$img".png" out/small/"out_"$img"_"$i"b.png" message.txt $i $1
+        ./lsbStegDiffuse in/"imag_"$img".png" out/small/"out_"$img"_"$i"b.png" message.txt $i $1 &
     done
 done
+
+wait
+echo "Done"
